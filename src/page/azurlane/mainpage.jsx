d5b99ffedbb32b956c4ad190d3c8026f13bb7244
dislido <909796264@qq.com>
2018-12-12 15:39:34 +0800

    增加砍口垒狗牌; 折叠狗牌图片; 进入网页时显示title为`加载中...`

--- src/page/azurlane/mainpage.jsx
+++ src/page/azurlane/mainpage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Card } from 'antd';
-import myinfImg from './img/myinf.jpg';
+import { Collapse } from 'antd';
+import myinfImg from './img/myinf.png';
 
 export default function Mainpage() {
   return (
     <div>
-      <Card title="狗牌">
-        <img src={myinfImg} style={{ maxWidth: '80%', maxHeight: '80%' }} alt="狗牌20181202" />
-      </Card>
+      <Collapse>
+        <Collapse.Panel header="狗牌">
+          <img src={myinfImg} alt="狗牌20181202" />
+        </Collapse.Panel>
+      </Collapse>
     </div>
   );
 }
--- src/page/kancolle/mainpage.jsx
+++ src/page/kancolle/mainpage.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Collapse } from 'antd';
+import myinfImg from './img/myinf.png';
 
 export default function Mainpage() {
   return (
     <div>
-      <Card title="我的提督信息">
-        <p>服务器：幌筵泊地</p>
-        <p>提督名：DisLiamn</p>
-        {/* <p>提督ID：15113194</p>
-        <p>nickNameId：130026293</p> */}
-      </Card>
+      <Collapse>
+        <Collapse.Panel header="狗牌">
+          <img src={myinfImg} alt="狗牌20181212" />
+        </Collapse.Panel>
+      </Collapse>
     </div>
   );
 }
